a939e882cd9524224128a8dfe497084f6700eff6
KBratland <kristin.bratland@gmail.com>
2014-11-17 15:09:20 -0800

    update Nov.16.14

---
+++ fruitlocate/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+rom django.contrib import admin
+from django.contrib.gis import admin
+from models import FruitLocate
+
+# Register your models here.
+admin.site.register(FruitLocate, admin.OSMGeoAdmin)
+
